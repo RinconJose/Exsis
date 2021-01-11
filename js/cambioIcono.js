4bf50db39b7322e820284fdb8e1a2f331d0dbb1a
Jose Rincon <rincon.jmg@gmail.com>
2021-01-11 15:24:08 -0500

    agregando el cambio del logo del menu dinamica con js

---
+++ js/cambioIcono.js
@@ -0,0 +1,5 @@
+function iconoDiferente(){
+    boton = document.getElementById("cambiarIcono");
+    // boton.classList.remove("navbar-toggler-icon");
+    boton.classList.toggle("navbar-toggler-icon--x");
+};
